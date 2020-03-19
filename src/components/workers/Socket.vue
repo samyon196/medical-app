7b86d738fd93f987ef718e67a615e7964a587880
samyon196 <samyon196@mail.com>
2020-03-19 15:43:10 +0200

    Sockets

---
+++ src/components/workers/Socket.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+
+<script>
+export default {
+    sockets: {
+        messages(payload) {
+            console.log('Got message from server!!!: ' + payload.txt);
+            console.log('IsSystem: ' + payload.systemMsg)
+            this.$store.dispatch('pushMessage', {txt: payload.txt, systemMsg: payload.systemMsg});
+            if(this.$route.name != "Chat") {
+                this.$store.dispatch('addUnreadMessage');
+            }
+        }
+    }
+}
+</script>
