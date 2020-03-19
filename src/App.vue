1b9d2312da993d4082334ef2a000cd135f9a0e6b
samyon196 <samyon196@mail.com>
2020-03-19 11:46:10 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,63 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<v-app class="grey lighten-4">
+    <v-navigation-drawer app clipped v-model="drawer" class="grey lighten-3">
+        <v-list dense>
+        <v-list-item link router :to="{name: 'Home'}" exact>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link router :to="{name: 'Chat'}" exact>
+          <v-list-item-action>
+            <v-badge v-show="msgNum > 0" overlap color="green" :content="msgNum" :value="msgNum">
+                <v-icon>mdi-message-text</v-icon>
+            </v-badge>
+            <v-icon v-show="msgNum == 0">mdi-message-text</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Chat</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link router :to="{name: 'Dashboard'}" exact>
+          <v-list-item-action>
+            <v-icon>mdi-monitor-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+        <v-toolbar-title>RF Chat App</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content class="grey lighten-4">
+        <v-container fluid>
+            <router-view></router-view>
+        </v-container>
+    </v-content>
+</v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+//import HelloWorld from './components/HelloWorld';
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    //HelloWorld,
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+    drawer: true
+  }),
+};
+</script>
