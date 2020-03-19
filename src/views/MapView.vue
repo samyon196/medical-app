42a1635b18b0958848f4424756605a799ab810c7
samyon196 <samyon196@mail.com>
2020-03-19 16:00:20 +0200

    add forcelist to mapview

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,14 +1,23 @@
 <template>
     <div>
-        Hello, map.
-        <Map />
+    <v-container>
+      <v-layout row>
+        <v-flex md3>
+          <ForcesList />
+        </v-flex>
+        <v-flex md9>
+            <Map />
+        </v-flex>
+      </v-layout> 
+    </v-container>
     </div>
 </template>
 
 <script>
 import Map from './../components/Map'
+import ForcesList from './../components/ForcesList'
 export default {
-    components: {Map}
+    components: {Map, ForcesList}
 }
 </script>
 
