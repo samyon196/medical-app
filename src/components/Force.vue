92878237871696ccaf1d13d60472f54004d2e71f
my kayost <mykayost@gmail.com>
2020-03-19 23:36:15 +0200

    added force with card option

--- src/components/Force.vue
+++ src/components/Force.vue
@@ -1,6 +1,7 @@
 <template>
 <div dir="rtl">
 <v-card
+    v-if="this.card"
     class="mx-auto"
     max-width="200"
     hover
@@ -16,13 +17,30 @@
       </v-list-item-content>
     </v-list-item>
   </v-card>
+  <div v-else>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-text v-if="this.event" style="color:red">באירוע</v-list-item-text>
+        <v-list-item-text v-else style="color:green">פנוי</v-list-item-text>
+        <v-list-item-title class="headline mb-1">{{name}}</v-list-item-title>
+        <v-list-item-text>סוג הכוח: {{type}}</v-list-item-text>
+        <v-list-item-subtitle v-if="this.event">משתתף באירוע: {{event}}</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </div>
 </div>
 </template>
 
 <script>
 export default {
     name: 'Force',
-        props: ['id', 'name', 'type', 'event']
+        props: {
+          card: Boolean,
+          id: Number,
+          name: String,
+          type: String,
+          event: String,
+        }
 }
 </script>
 
