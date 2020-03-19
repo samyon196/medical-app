0b44c64d6667cc94f13d969b623ee8f1f73b1b5d
Maor <hashlama5559991@gmail.com>
2020-03-19 14:38:32 +0200

    Fixed the title of the app

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
 
     <v-app-bar app clipped-left>
         <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-        <v-toolbar-title>RF Chat App</v-toolbar-title>
+        <v-toolbar-title>Medical App</v-toolbar-title>
     </v-app-bar>
 
     <v-content class="grey lighten-4">
